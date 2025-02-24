a3c6c6bf90242f2ccd738d4e13d975cba212d897
Marc Hershey <marchershey@users.noreply.github.com>
2025-02-24 01:17:53 -0500

    chore: dep update

--- app/resources/views/layouts/base.blade.php
+++ app/resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
-    @fluxStyles
+    @fluxAppearance
     {{-- <link href="https://fonts.gstatic.com" rel="preconnect"> --}}
     <link href="https://fonts.bunny.net" rel="preconnect">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800,900&display=swap" rel="stylesheet" />
